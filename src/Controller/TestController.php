08a62c8338383ca93e0d18e9e66cb29e3ff65bf6
Marco Lipparini <developer@liarco.net>
2020-10-30 13:14:50 +0100

    Fixing bad PHP examples

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -82,22 +82,21 @@ class TestController extends AbstractController
                 ], 'my-domain'),
             ],
             'unsupported_translations' => [
-                t('D_php_t_empty'),
-                t('D_php_t_empty_array', []),
-                t('D_php_t_with_param_one_line', ['var1' => 'val1']),
-                t('D_php_t_with_params_one_line_no_trailing_comma', ['var1' => 'val1', 'var2' => 'val2']),
-                t('D_php_t_with_params_one_line_with_trailing_comma', ['var1' => 'val1', 'var2' => 'val2',]),
+                t('D_php_t_empty_array', [], 'my-domain'),
+                t('D_php_t_with_param_one_line', ['var1' => 'val1'], 'my-domain'),
+                t('D_php_t_with_params_one_line_no_trailing_comma', ['var1' => 'val1', 'var2' => 'val2'], 'my-domain'),
+                t('D_php_t_with_params_one_line_with_trailing_comma', ['var1' => 'val1', 'var2' => 'val2',], 'my-domain'),
                 t('D_php_t_with_param_with_whitespaces', [
                     'var1' => 'val1'
-                ]),
+                ], 'my-domain'),
                 t('D_php_t_with_params_with_whitespaces_no_trailing_comma', [
                     'var1' => 'val1',
                     'var2' => 'val2'
-                ]),
+                ], 'my-domain'),
                 t('D_php_t_with_params_with_whitespaces_with_trailing_comma', [
                     'var1' => 'val1',
                     'var2' => 'val2',
-                ]),
+                ], 'my-domain'),
             ],
         ]);
     }
