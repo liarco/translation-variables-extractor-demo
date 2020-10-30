d4881648a1bf7099050a086ea1b4b36c8aa7dcbb
Marco Lipparini <developer@liarco.net>
2020-10-30 18:47:48 +0100

    Supporting both long and short array syntax (PHP extractor)

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -18,43 +18,74 @@ class TestController extends AbstractController
      */
     public function index(TranslatorInterface $translator): Response
     {
+
         return $this->render('test/index.html.twig', [
             'supported_translations' => [
                 // $translator->trans(...)
                 $translator->trans('ND_php_trans_empty'),
                 $translator->trans('ND_php_trans_empty_array', []),
+                $translator->trans('ND_php_trans_empty_array_long_array', array()),
                 $translator->trans('ND_php_trans_with_param_one_line', ['var1' => 'val1']),
+                $translator->trans('ND_php_trans_with_param_one_line_long_array', array('var1' => 'val1')),
                 $translator->trans('ND_php_trans_with_params_one_line_no_trailing_comma', ['var1' => 'val1', 'var2' => 'val2']),
+                $translator->trans('ND_php_trans_with_params_one_line_no_trailing_comma_long_array', array('var1' => 'val1', 'var2' => 'val2')),
                 $translator->trans('ND_php_trans_with_params_one_line_with_trailing_comma', ['var1' => 'val1', 'var2' => 'val2',]),
+                $translator->trans('ND_php_trans_with_params_one_line_with_trailing_comma_long_array', array('var1' => 'val1', 'var2' => 'val2',)),
                 $translator->trans('ND_php_trans_with_param_with_whitespaces', [
                     'var1' => 'val1'
                 ]),
+                $translator->trans('ND_php_trans_with_param_with_whitespaces_long_array', array(
+                    'var1' => 'val1'
+                )),
                 $translator->trans('ND_php_trans_with_params_with_whitespaces_no_trailing_comma', [
                     'var1' => 'val1',
                     'var2' => 'val2'
                 ]),
+                $translator->trans('ND_php_trans_with_params_with_whitespaces_no_trailing_comma_long_array', array(
+                    'var1' => 'val1',
+                    'var2' => 'val2'
+                )),
                 $translator->trans('ND_php_trans_with_params_with_whitespaces_with_trailing_comma', [
                     'var1' => 'val1',
                     'var2' => 'val2',
                 ]),
+                $translator->trans('ND_php_trans_with_params_with_whitespaces_with_trailing_comma_long_array', array(
+                    'var1' => 'val1',
+                    'var2' => 'val2',
+                )),
 
                 // t(...)->trans($translator)
                 t('ND_php_t_empty')->trans($translator),
                 t('ND_php_t_empty_array', [])->trans($translator),
+                t('ND_php_t_empty_array_long_array', array())->trans($translator),
                 t('ND_php_t_with_param_one_line', ['var1' => 'val1'])->trans($translator),
+                t('ND_php_t_with_param_one_line_long_array', array('var1' => 'val1'))->trans($translator),
                 t('ND_php_t_with_params_one_line_no_trailing_comma', ['var1' => 'val1', 'var2' => 'val2'])->trans($translator),
+                t('ND_php_t_with_params_one_line_no_trailing_comma_long_array', array('var1' => 'val1', 'var2' => 'val2'))->trans($translator),
                 t('ND_php_t_with_params_one_line_with_trailing_comma', ['var1' => 'val1', 'var2' => 'val2',])->trans($translator),
+                t('ND_php_t_with_params_one_line_with_trailing_comma_long_array', array('var1' => 'val1', 'var2' => 'val2',))->trans($translator),
                 t('ND_php_t_with_param_with_whitespaces', [
                     'var1' => 'val1'
                 ])->trans($translator),
+                t('ND_php_t_with_param_with_whitespaces_long_array', array(
+                    'var1' => 'val1'
+                ))->trans($translator),
                 t('ND_php_t_with_params_with_whitespaces_no_trailing_comma', [
                     'var1' => 'val1',
                     'var2' => 'val2'
                 ])->trans($translator),
+                t('ND_php_t_with_params_with_whitespaces_no_trailing_comma_long_array', array(
+                    'var1' => 'val1',
+                    'var2' => 'val2'
+                ))->trans($translator),
                 t('ND_php_t_with_params_with_whitespaces_with_trailing_comma', [
                     'var1' => 'val1',
                     'var2' => 'val2',
                 ])->trans($translator),
+                t('ND_php_t_with_params_with_whitespaces_with_trailing_comma_long_array', array(
+                    'var1' => 'val1',
+                    'var2' => 'val2',
+                ))->trans($translator),
             ],
             'unsupported_translations' => [
             ],
@@ -68,38 +99,68 @@ class TestController extends AbstractController
         return $this->render('test/domain.html.twig', [
             'supported_translations' => [
                 // $translator->trans(...)
-                $translator->trans('D_php_trans_empty', [], 'my-domain'),
+                $translator->trans('D_php_trans_empty_array', [], 'my-domain'),
+                $translator->trans('D_php_trans_empty_array_long_array', array(), 'my-domain'),
                 $translator->trans('D_php_trans_with_param_one_line', ['var1' => 'val1'], 'my-domain'),
+                $translator->trans('D_php_trans_with_param_one_line_long_array', array('var1' => 'val1'), 'my-domain'),
                 $translator->trans('D_php_trans_with_params_one_line_no_trailing_comma', ['var1' => 'val1', 'var2' => 'val2'], 'my-domain'),
+                $translator->trans('D_php_trans_with_params_one_line_no_trailing_comma_long_array', array('var1' => 'val1', 'var2' => 'val2'), 'my-domain'),
                 $translator->trans('D_php_trans_with_params_one_line_with_trailing_comma', ['var1' => 'val1', 'var2' => 'val2',], 'my-domain'),
+                $translator->trans('D_php_trans_with_params_one_line_with_trailing_comma_long_array', array('var1' => 'val1', 'var2' => 'val2',), 'my-domain'),
                 $translator->trans('D_php_trans_with_param_with_whitespaces', [
                     'var1' => 'val1'
                 ], 'my-domain'),
+                $translator->trans('D_php_trans_with_param_with_whitespaces_long_array', array(
+                    'var1' => 'val1'
+                ), 'my-domain'),
                 $translator->trans('D_php_trans_with_params_with_whitespaces_no_trailing_comma', [
                     'var1' => 'val1',
                     'var2' => 'val2'
                 ], 'my-domain'),
+                $translator->trans('D_php_trans_with_params_with_whitespaces_no_trailing_comma_long_array', array(
+                    'var1' => 'val1',
+                    'var2' => 'val2'
+                ), 'my-domain'),
                 $translator->trans('D_php_trans_with_params_with_whitespaces_with_trailing_comma', [
                     'var1' => 'val1',
                     'var2' => 'val2',
                 ], 'my-domain'),
+                $translator->trans('D_php_trans_with_params_with_whitespaces_with_trailing_comma_long_array', array(
+                    'var1' => 'val1',
+                    'var2' => 'val2',
+                ), 'my-domain'),
 
                 // t(...)->trans($translator)
                 t('D_php_t_empty_array', [], 'my-domain')->trans($translator),
+                t('D_php_t_empty_array_long_array', array(), 'my-domain')->trans($translator),
                 t('D_php_t_with_param_one_line', ['var1' => 'val1'], 'my-domain')->trans($translator),
+                t('D_php_t_with_param_one_line_long_array', array('var1' => 'val1'), 'my-domain')->trans($translator),
                 t('D_php_t_with_params_one_line_no_trailing_comma', ['var1' => 'val1', 'var2' => 'val2'], 'my-domain')->trans($translator),
+                t('D_php_t_with_params_one_line_no_trailing_comma_long_array', array('var1' => 'val1', 'var2' => 'val2'), 'my-domain')->trans($translator),
                 t('D_php_t_with_params_one_line_with_trailing_comma', ['var1' => 'val1', 'var2' => 'val2',], 'my-domain')->trans($translator),
+                t('D_php_t_with_params_one_line_with_trailing_comma_long_array', array('var1' => 'val1', 'var2' => 'val2',), 'my-domain')->trans($translator),
                 t('D_php_t_with_param_with_whitespaces', [
                     'var1' => 'val1'
                 ], 'my-domain')->trans($translator),
+                t('D_php_t_with_param_with_whitespaces_long_array', array(
+                    'var1' => 'val1'
+                ), 'my-domain')->trans($translator),
                 t('D_php_t_with_params_with_whitespaces_no_trailing_comma', [
                     'var1' => 'val1',
                     'var2' => 'val2'
                 ], 'my-domain')->trans($translator),
+                t('D_php_t_with_params_with_whitespaces_no_trailing_comma_long_array', array(
+                    'var1' => 'val1',
+                    'var2' => 'val2'
+                ), 'my-domain')->trans($translator),
                 t('D_php_t_with_params_with_whitespaces_with_trailing_comma', [
                     'var1' => 'val1',
                     'var2' => 'val2',
                 ], 'my-domain')->trans($translator),
+                t('D_php_t_with_params_with_whitespaces_with_trailing_comma_long_array', array(
+                    'var1' => 'val1',
+                    'var2' => 'val2',
+                ), 'my-domain')->trans($translator),
             ],
             'unsupported_translations' => [
             ],
